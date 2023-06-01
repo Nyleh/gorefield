f72600e1c7cd00511863e10ec68f2c42b76cebc4
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-05-31 20:00:48 -0700

    First Commit

---
+++ data/charts/healthBarColors.hx
@@ -0,0 +1,7 @@
+function postCreate() {
+    var bfColor = (boyfriend != null && boyfriend.xml != null && boyfriend.xml.exists("color")) ? CoolUtil.getColorFromDynamic(boyfriend.xml.get("color")) : 0xFF66FF33;
+    var dadColor = (dad != null && dad.xml != null && dad.xml.exists("color")) ? CoolUtil.getColorFromDynamic(dad.xml.get("color")) : 0xFFFF0000;
+
+    healthBar.createFilledBar(dadColor, bfColor);
+    iconP2.y -= 7.5; // keeps me sane
+}
---
+++ data/charts/stageZooming.hx
@@ -0,0 +1,18 @@
+var strumLineZooms:Map<Int, Float> = [];
+
+function create() {
+    if (stage == null || stage.stageXML == null) return;
+
+    var zoomNames:Array<String> = ["opponentZoom", "playerZoom", "gfZoom"];
+    for (zoom in zoomNames)
+        if (stage.stageXML.exists(zoom)) strumLineZooms.set(zoomNames.indexOf(zoom), Std.parseFloat(stage.stageXML.get(zoom)));
+
+    if (strumLineZooms.exists(curCameraTarget)) defaultCamZoom = strumLineZooms.get(curCameraTarget);
+}
+
+function onEvent(codenameEvent) {
+    var event = codenameEvent.event;
+    switch(event.name) {
+        case "Camera Movement": if (strumLineZooms.exists(event.params[0])) defaultCamZoom = strumLineZooms.get(event.params[0]);
+    }
+}
