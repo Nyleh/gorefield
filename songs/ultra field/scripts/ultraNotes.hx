dce84d847ff4c969e34f1a7e8f6dd1836e1985a9
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-06-05 14:50:19 -0700

    some more changes

--- songs/ultra field/scripts/ultraNotes.hx
+++ songs/ultra field/scripts/ultraNotes.hx
@@ -7,7 +7,7 @@ var glowShader:CustomShader;
 function create() {
     for (cam in [camGame, camHUD]) FlxG.cameras.remove(cam, false);
 
-    ultraCam = new FlxCamera(0,FlxG.height/2 - FlxG.width/2, FlxG.width + 200, FlxG.width + 200);
+    ultraCam = new FlxCamera(0,FlxG.height/2 - FlxG.width/2, FlxG.width, FlxG.width);
     ultraCam.angle = 90;
     
     chromaticWarpShader = new CustomShader("chromaticWarp");
@@ -30,6 +30,20 @@ function postCreate() {
         strum.y = 0;
         strum.cameras = [ultraCam];
     }
+}
+
+var dimTween:FlxTween;
+var sizeTween:FlxTween;
+function measureHit() {
+    for (tween in [dimTween, sizeTween]) if (tween != null) tween.cancel();
+
+    dimTween = FlxTween.num(0.2, 0.6, (Conductor.stepCrochet / 1000) * 4, {}, (val:Float) -> {
+        glowShader.dim = val;
+    });
+
+    sizeTween = FlxTween.num(80.0, 50.0, (Conductor.stepCrochet / 1000) * 4, {}, (val:Float) -> {
+        glowShader.size = val;
+    });
 }
 
 function onStrumCreation(strumEvent) if (strumEvent.player == 0) strumEvent.__doAnimation = false;
