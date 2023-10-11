d450d57110a0a0cfb4c5983bde42f53206723901
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-10 23:47:07 -0700

    add guitar note sprite

---
+++ data/notes/Guitar Note.hx
@@ -0,0 +1,16 @@
+function onNoteCreation(event) {
+    /*
+    if (event.noteType != "Guitar Note") return;
+    event.cancel(true); // stop continued calls to other scripts
+
+    event.note.frames = Paths.getFrames(event.noteSprite);
+    switch(event.strumID % 4) {
+        case 0: event.note.animation.addByPrefix('scroll', 'orange note left');
+        case 1: event.note.animation.addByPrefix('scroll', 'orange note down');
+        case 2: event.note.animation.addByPrefix('scroll', 'orange note up');
+        case 3: event.note.animation.addByPrefix('scroll', 'orange note right');
+    }
+
+    event.note.scale.set(event.noteScale, event.noteScale);
+    event.note.antialiasing = true;*/
+}
