75efb3decb8b0752ef04196e3c02f5fcb3c82400
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-11 19:15:18 -0700

    better ps note visualizer

--- data/notes/PS Note.hx
+++ data/notes/PS Note.hx
@@ -14,20 +14,23 @@ function postCreate() {
 
     psBarTrail = new FlxTrail(psBar, null, 4, 100, 0.5, 0.069);
     psBarTrail.cameras = [camHUD];
-    psBarTrail.color = 0xFFFF0000;
+    psBarTrail.color = 0xFFFF0000; psBarTrail.alpha = 0;
     psBarTrail.active = psBarTrail.visible = false;
 
     add(psBarTrail);
     add(psBar);
-} //
+}
 
 var fullTime:Float = 0;
 function update(elapsed:Float) {
     if (ps <= 2) {
         fullTime += elapsed;
-        psBar.x = 60 + (6*Math.sin(fullTime)) + FlxG.random.float(0,1.5);
-        psBar.y = 385 + (4*Math.cos(fullTime)) + FlxG.random.float(0,1.4);
+        psBar.x = lerp(psBar.x, 60 + (6*Math.sin(fullTime)), 1/4) + FlxG.random.float(0, ps == 1 ? .5 : .4);
+        psBar.y = lerp(psBar.y, 385 + (4*Math.cos(fullTime)), 1/4) + FlxG.random.float(0, ps == 1 ? .5 : .4);
+        psBar.color = FlxColor.interpolate(psBar.color, ps == 1 ?0x6CFF6A6A : 0xFFFFC8C8, 1/14);
+
         psBarTrail.active = psBarTrail.visible = true;
+        psBarTrail.alpha = lerp(psBarTrail.alpha, 0.4, 1/4);
     }
 }
 
