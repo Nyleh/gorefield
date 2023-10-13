4a9df89fc8c77541b12bac2ab2a70344f29ad012
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-13 00:55:08 -0700

    fix some bg color glitches

--- data/global.hx
+++ data/global.hx
@@ -9,9 +9,10 @@ static var redirectStates:Map<FlxState, String> = [
 ];
 
 function preStateSwitch() {
+    FlxG.camera.bgColor = 0xFF000000;
     for (redirectState in redirectStates.keys()) 
         if (Std.isOfType(FlxG.game._requestedState, redirectState)) 
             FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
 }
 
-function destroy() FlxG.camera.bgColor = 0xFF000000;
+function onDestroy() FlxG.camera.bgColor = 0xFF000000;
