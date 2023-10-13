893b414e512da7f17d0fe7510dd3e212664627ca
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-12 23:24:02 -0700

    fix splashes again

--- data/notes/BW Note.hx
+++ data/notes/BW Note.hx
@@ -1,5 +1,5 @@
 function onNoteCreation(event) {
     if (event.noteType != "BW Note") return;
     event.noteSprite = "game/notes/gorefield_bw";
-    // TODO: ADD SPLASHES WHEN THEY GET DRAWN
+    event.note.splash = "gorefield_bw";
 }
