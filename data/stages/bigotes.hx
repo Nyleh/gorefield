7bb7d523d07005c21ae9aee6c5e5afebb37e1e5b
NeeEoo <NeeEoo@users.noreply.github.com>
2023-10-13 04:30:43 +0200

    Fixed trail on bigotes

--- data/stages/bigotes.hx
+++ data/stages/bigotes.hx
@@ -6,18 +6,24 @@ var trailBloom:CustomShader;
 
 function create() {
     jonTrail = new FlxTrail(dad, null, 4, 10, 0.4, 0.069);
+    jonTrail.beforeCache = dad.beforeTrailCache;
+    jonTrail.afterCache = () -> {
+		dad.afterTrailCache();
+		//jonTrail.members[0].x += FlxG.random.float(-5, 5);
+		//jonTrail.members[0].y += FlxG.random.float(-5, 5);
+	}
     jonTrail.color = 0xFFF200FF;
 
     trailBloom = new CustomShader("glow");
-    trailBloom.size = 18.0; trailBloom.quality = 8.0;
-    trailBloom.dim = 0.8; trailBloom.directions = 16.0;
+    trailBloom.size = 18.0;// trailBloom.quality = 8.0;
+    trailBloom.dim = 0.8;// trailBloom.directions = 16.0;
 
     for (trailSpr in jonTrail.members) {
         trailSpr.shader = trailBloom;
-        trailSpr.animation.callback = (name:String, frameNumber:Int, frameIndex:Int) -> {
-            if (dad != null && dad.animOffsets.exists(trailSpr.animation.name)) 
-                trailSpr.offset.set(dad.animOffsets[trailSpr.animation.name].x, dad.animOffsets[trailSpr.animation.name].y);
-        };
+        //trailSpr.animation.callback = (name:String, frameNumber:Int, frameIndex:Int) -> {
+        //    if (dad != null && dad.animOffsets.exists(trailSpr.animation.name))
+        //        trailSpr.offset.set(dad.animOffsets[trailSpr.animation.name].x, dad.animOffsets[trailSpr.animation.name].y);
+        //};
     }
 
     insert(members.indexOf(dad), jonTrail);
