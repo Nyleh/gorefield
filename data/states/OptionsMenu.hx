1a9bce5c2d7e79d89b0e9f7b4560d96fa055cf3f
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-13 13:10:59 -0700

    options fit the menus

---
+++ data/states/OptionsMenu.hx
@@ -0,0 +1,16 @@
+
+function postCreate() {
+    var bg:FlxSprite = new FlxSprite().makeSolid(FlxG.width, FlxG.height, FlxColor.fromRGB(17,5,33));
+    bg.scrollFactor.set(0,0);
+    insert(1, bg); // 1 is right after bg WHICH IS NOT A VAR FOR SOME REASON!!!11
+
+    var logoBl:FlxSprite = new FlxSprite(1020, 6);
+	logoBl.frames = Paths.getSparrowAtlas('menus/logoMod');
+	logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
+	logoBl.animation.play('bump');
+	logoBl.scale.set(0.3, 0.3);
+    logoBl.scrollFactor.set(0,0);
+	logoBl.updateHitbox();
+	logoBl.antialiasing = true;
+	add(logoBl);
+}
