2f017051d7fbbc962913f7072ba2a9e18cd6242e
Jloor <gamerjloor@gmail.com>
2023-10-20 10:42:54 -0500

    Polish Fast Delivery.,.,.,. frrr

---
+++ songs/fast delivery/scripts/events_song.hx
@@ -0,0 +1,43 @@
+var lasagnaCat:FlxSprite;
+
+function create()
+{
+	if (PlayState.SONG.meta.name.toLowerCase() == "fast delivery")
+	{
+		lasagnaCat = new FlxSprite(-1600, 60);
+		lasagnaCat.frames = Paths.getSparrowAtlas('stages/pixelLasagna/LASAGNA_CAT_BG');
+		lasagnaCat.animation.addByPrefix('run', 'LASAGNA CAT RUN BG', 12, false);
+		lasagnaCat.visible = false;
+		lasagnaCat.flipX = true;
+		lasagnaCat.color = 0xFF000000;
+		add(lasagnaCat);
+	}
+	else
+		__script__.didLoad = __script__.active = false;
+}
+
+function stepHit(step:Int) 
+{
+    switch (step) 
+    {
+        case 512:
+			maxCamZoom = 0;
+		case 1024:
+			lasagnaCat.visible = true;
+			lasagnaCat.animation.play('run', false);
+		case 1056:
+			lasagnaCat.visible = false;
+		case 1104 | 1112 | 1168 | 1176 | 1232 | 1240 | 1296 | 1304:
+			camHUD.angle -= 10;
+			camGame.angle -= 3.5;
+			camHUD.y += 5;
+			FlxTween.tween(camHUD, {angle: 0, y: 0}, 0.3, {ease: FlxEase.quadOut});
+			FlxTween.tween(camGame, {angle: 0}, 0.3, {ease: FlxEase.quadOut});
+		case 1108 | 1116 | 1172 | 1180 | 1236 | 1244 | 1300 | 1308:
+			camHUD.angle += 10;
+			camGame.angle += 3.5;
+			camHUD.y -= 5;
+			FlxTween.tween(camHUD, {angle: 0, y: 0}, 0.3, {ease: FlxEase.quadOut});
+			FlxTween.tween(camGame, {angle: 0}, 0.3, {ease: FlxEase.quadOut});
+    }
+}
