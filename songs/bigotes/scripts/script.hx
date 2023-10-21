54cc038ea5583127e79036ba46e16ba0436dc0d5
Lean <84594436+LeanDapper@users.noreply.github.com>
2023-10-20 23:01:30 -0700

    ya

--- songs/bigotes/scripts/script.hx
+++ songs/bigotes/scripts/script.hx
@@ -1,16 +1,29 @@
 function create()
 {
-    stage.stageSprites["black"].cameras = [camHUD];
+    //stage.stageSprites["black"].cameras = [camHUD];
     stage.stageSprites["pixelblack"].drawComplex(FlxG.camera);
+}
+
+function postCreate()
+{
+    lerpCam = false;
+    defaultCamZoom = 1.5;
+    FlxG.camera.zoom = 1.5;
+    camHUD.alpha = 0.01;
 }
 
 function stepHit(step:Int) 
 {
     switch (step)
     {
-        case 1:
+        case 0:
+            FlxTween.tween(FlxG.camera, {zoom: 0.72}, (Conductor.stepCrochet / 1000) * 30, {onComplete: function (tween:FlxTween) {defaultCamZoom = 0.72; lerpCam = true;}});
             stage.stageSprites["black"].active = stage.stageSprites["black"].visible = true;
             FlxTween.tween(stage.stageSprites["black"], {alpha: 0}, 3);
+        case 32:
+            FlxG.camera.zoom += 0.1;
+            camHUD.zoom += 0.03;
+            FlxTween.tween(camHUD,{alpha: 1},0.5);
         case 1456:
             camGame.flash('0xFFFFFFFF', 1);
             stage.stageSprites["BIGOTESBG"].visible = false;
