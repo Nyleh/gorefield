242edd94c569f77877d58b3281854e94976e91e6
Jloor <gamerjloor@gmail.com>
2023-10-20 23:49:11 -0500

    Update Pixel

---
+++ data/scripts/pixel-gorefield.hx
@@ -0,0 +1,82 @@
+import funkin.game.HudCamera;
+import funkin.backend.scripting.events.NoteHitEvent;
+
+public var pixelNotesForBF = true;
+public var pixelNotesForDad = true;
+public var enablePixelUI = true;
+public var enablePixelGameOver = true;
+public var enableCameraHacks = Options.week6PixelPerfect;
+
+var oldStageQuality = FlxG.game.stage.quality;
+static var daPixelZoom = 6;
+
+/**
+ * UI
+ */
+function onNoteCreation(event) {
+	if (event.note.strumLine == playerStrums && !pixelNotesForBF) return;
+	if (event.note.strumLine == cpuStrums && !pixelNotesForDad) return;
+
+	event.cancel();
+
+	var note = event.note;
+	if (event.note.isSustainNote) {
+		note.loadGraphic(Paths.image('game/pixel/Notes_Assets_End'), true, 7, 6);
+		note.animation.add("hold", [event.strumID]);
+		note.animation.add("holdend", [4 + event.strumID]);
+	} else {
+		note.loadGraphic(Paths.image('game/pixel/Notes'), true, 17, 17);
+		note.animation.add("scroll", [4 + event.strumID]);
+	}
+	note.scale.set(daPixelZoom, daPixelZoom);
+	note.updateHitbox();
+}
+
+function onStrumCreation(event) {
+	if (event.player == 1 && !pixelNotesForBF) return;
+	if (event.player == 0 && !pixelNotesForDad) return;
+
+	event.cancel();
+
+	var strum = event.strum;
+	strum.loadGraphic(Paths.image('game/pixel/Notes'), true, 17, 17);
+	strum.animation.add("static", [event.strumID]);
+	strum.animation.add("pressed", [4 + event.strumID, 8 + event.strumID], 12, false);
+	strum.animation.add("confirm", [12 + event.strumID, 16 + event.strumID], 24, false);
+
+	strum.scale.set(daPixelZoom, daPixelZoom);
+	strum.updateHitbox();
+}
+
+/*function onCountdown(event) {
+	if (!enablePixelUI) return;
+
+	event.antialiasing = false;
+	event.scale = daPixelZoom;
+	event.spritePath = switch(event.swagCounter) {
+		case 0: null;
+		case 1: 'stages/school/ui/ready';
+		case 2: 'stages/school/ui/set';
+		case 3: 'stages/school/ui/go';
+	};
+}*/
+
+function onPlayerHit(event:NoteHitEvent) {
+	if (!enablePixelUI) return;
+	event.ratingPrefix = "game/pixel/";
+	event.ratingScale = daPixelZoom * 0.7;
+	event.ratingAntialiasing = false;
+
+	event.numScale = daPixelZoom;
+	event.numAntialiasing = false;
+}
+
+function postCreate() {
+	iconP1.antialiasing = false;
+	iconP2.antialiasing = false;
+
+	if (enablePixelGameOver) {
+		gameOverSong = "gameOvers/lasagna/Gorefield_Gameover_Pixel";
+		retrySFX = "gameOvers/lasagna/Continue_Pixel";
+	}
+}
--- data/stages/pixelLasagna.hx
+++ data/stages/pixelLasagna.hx
@@ -1,7 +1,6 @@
 function create() 
 {
-	gameOverSong = "gameOvers/lasagna/Gorefield_Gameover_Pixel";
-	retrySFX = "gameOvers/lasagna/Continue_Pixel";
+	importScript("data/scripts/pixel-gorefield");
 
 	__script__.didLoad = __script__.active = false;
 }
