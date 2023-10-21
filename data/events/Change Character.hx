52a7fc0b0fc3528df83eb50305df279d3b371f1a
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-21 13:38:29 -0700

    fix stupid change character event (bigotes trail)

--- data/events/Change Character.hx
+++ data/events/Change Character.hx
@@ -1,9 +1,21 @@
 var preloadedCharacters:Map<String, Character> = [];
 var preloadedIcons:Map<String, FlxSprite> = [];
 
-function create() {
+function postCreate() {
     for (event in PlayState.SONG.events)
         if (event.name == "Change Character" && !preloadedCharacters.exists(event.params[1])) {
+            // Look for character that alreadly exists
+            var foundPreExisting:Bool = false;
+            for (strum in strumLines) 
+                for (char in strum.characters)
+                    if (char.curCharacter == event.params[1]) {
+                        preloadedCharacters.set(event.params[1], char);
+                        preloadedIcons.set(char.getIcon(), char == dad ? gorefieldiconP2 : gorefieldiconP1);
+                        foundPreExisting = true; break;
+                    }
+            if (foundPreExisting) continue;
+
+            // Create New Character
             var oldCharacter = strumLines.members[event.params[0]].characters[0];
             var newCharacter = new Character(oldCharacter.x, oldCharacter.y, event.params[1], oldCharacter.isPlayer);
             newCharacter.active = newCharacter.visible = false;
@@ -11,22 +23,18 @@ function create() {
             preloadedCharacters.set(event.params[1], newCharacter);
             
             //Adjust Camera Offset to Accomedate Stage Offsets
-            if(newCharacter.isGF){
-                trace('Is GF ' + newCharacter.curCharacter);
+            if(newCharacter.isGF) {
                 newCharacter.cameraOffset.x += stage.characterPoses["gf"].camxoffset;
                 newCharacter.cameraOffset.y += stage.characterPoses["gf"].camyoffset;
-            }
-            else if(newCharacter.playerOffsets){
-                trace('Is BF ' + newCharacter.curCharacter);
+            } else if(newCharacter.playerOffsets){
                 newCharacter.cameraOffset.x += stage.characterPoses["boyfriend"].camxoffset;
                 newCharacter.cameraOffset.y += stage.characterPoses["boyfriend"].camyoffset;
-            }
-            else{
-                trace('Is DAD ' + newCharacter.curCharacter);
+            } else {
                 newCharacter.cameraOffset.x += stage.characterPoses["dad"].camxoffset;
                 newCharacter.cameraOffset.y += stage.characterPoses["dad"].camyoffset;
             }
             
+            // Create New Icon
             if (preloadedIcons.exists(newCharacter.getIcon())) continue;
             var newIcon = createIcon(newCharacter); // ! GOREFIELD ONLY
             newIcon.active = newIcon.visible = false;
@@ -38,6 +46,7 @@ function create() {
 function onEvent(_) {
     var params:Array = _.event.params;
     if (_.event.name == "Change Character") {
+        // Change Character
         var oldCharacter = strumLines.members[params[0]].characters[0];
         var newCharacter = preloadedCharacters.get(params[1]);
         if (oldCharacter.curCharacter == newCharacter.curCharacter) return;
@@ -51,6 +60,7 @@ function onEvent(_) {
         newCharacter.animation?.curAnim?.curFrame = oldCharacter.animation?.curAnim?.curFrame;
         strumLines.members[params[0]].characters[0] = newCharacter;
 
+        // Change Icon
         var oldIcon = oldCharacter.isPlayer ? gorefieldiconP1 : gorefieldiconP2; // ! GOREFIELD ONLY
         var newIcon = preloadedIcons.get(newCharacter.getIcon());
 
@@ -61,7 +71,5 @@ function onEvent(_) {
         if (oldCharacter.isPlayer) gorefieldiconP1 = newIcon;
         else gorefieldiconP2 = newIcon;
         updateIcons(); 
-
-        //if (newCharacter == dad) timeBar.createFilledBar(0xFF000000, newCharacter.iconColor);
     }
 }
