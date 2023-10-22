f3409863a562a41d5453bb60257bbaf1e9377cec
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-22 12:54:11 -0700

    i love shader

--- songs/curious cat/scripts/black_screen.hx
+++ songs/curious cat/scripts/black_screen.hx
@@ -1,6 +1,8 @@
 var black:FlxSprite; // It's not completely black, but weell... -EstoyAburridow
 var blackVignette:FlxSprite;
 
+var staticShader:CustomShader;
+
 function create()
 {
     if (PlayState.SONG.meta.name.toLowerCase() == "curious cat")
@@ -15,6 +17,11 @@ function create()
 
         stage.stageSprites["BlackVignette"].drawComplex(FlxG.camera);
         stage.stageSprites["BlackVignette"].cameras = [camHUD];
+
+        staticShader = new CustomShader("tvstatic");
+        staticShader.time = 0; staticShader.strength = 0;
+        staticShader.speed = 20;
+        FlxG.camera.addShader(staticShader);
     }
     else
         __script__.didLoad = __script__.active = false;
@@ -28,37 +35,44 @@ function tweenHUD(fadeIn:Bool, time:Float){ //this is so i dont have to write th
         FlxTween.tween(strumNote,{alpha: fadeIn ? 1 : 0},time);
     }
 
-    FlxTween.tween(gorefieldhealthBarBG,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(gorefieldhealthBar,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(gorefieldiconP1,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(gorefieldiconP2,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(scoreTxt,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(missesTxt,{alpha: fadeIn ? 1 : 0},time);
-    FlxTween.tween(accuracyTxt,{alpha: fadeIn ? 1 : 0},time);
+    for (spr in [gorefieldhealthBarBG, gorefieldhealthBar, gorefieldiconP1, gorefieldiconP2, scoreTxt, missesTxt, accuracyTxt])
+        FlxTween.tween(spr,{alpha: fadeIn ? 1 : 0},time);
 }
+
 var dadXOffset:Float;
 function postCreate(){
     dadXOffset = dad.cameraOffset.x;
 }
 
+var totalTime:Float = 0;
+function update(elapsed:Float) {
+    totalTime += elapsed;
+    staticShader.time = totalTime;
+}
+
 function stepHit(step:Int) 
 {
-    switch (step) 
-    {
+    switch (step) {
         case 140:
             black.active = black.visible = false;
-
 			maxCamZoom = 0;
         case 656:
             tweenHUD(false,1);
             lerpCam = false;
-            FlxTween.tween(FlxG.camera, {zoom: FlxG.camera.zoom + 0.7},15);
-            FlxTween.tween(dad.cameraOffset, {x: dadXOffset - 260}, 8);
-            FlxTween.tween(stage.stageSprites["BlackVignette"], {alpha: 1},16);
-            camGame.shake(0.0013, 15);
+            FlxTween.tween(FlxG.camera, {zoom: FlxG.camera.zoom + 0.3}, 15, {ease: FlxEase.qaudInOut});
+            FlxTween.tween(dad.cameraOffset, {x: dadXOffset - 260}, 16, {ease: FlxEase.linear});
+            FlxTween.tween(stage.stageSprites["BlackVignette"], {alpha: 1},16, {ease: FlxEase.quadOut});
+            FlxTween.num(0.0, 1.3, 16, {ease: FlxEase.quadOut}, (val:Float) -> {
+                staticShader.strength = val;
+            });
+            (new FlxTimer()).start(6, function () {camGame.shake(0.0005, 15-6);});
+            (new FlxTimer()).start(6+6, function () {camGame.shake(0.0012, 17-6-6);});
         case 780:
-            tweenHUD(true,0.3);
+            tweenHUD(true, 0.3);
             FlxTween.tween(stage.stageSprites["BlackVignette"], {alpha: 0},1);
+            FlxTween.num(1.3, 0., 1, {ease: FlxEase.quadOut}, (val:Float) -> {
+                staticShader.strength = val;
+            });
         case 783:
             dad.cameraOffset.x = dadXOffset;
             lerpCam = true;
