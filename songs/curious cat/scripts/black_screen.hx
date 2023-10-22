677fa730cade979b5183f1bb9154619e29eca2df
Lean <84594436+LeanDapper@users.noreply.github.com>
2023-10-22 11:29:29 -0700

    curious cat events finished

--- songs/curious cat/scripts/black_screen.hx
+++ songs/curious cat/scripts/black_screen.hx
@@ -1,4 +1,5 @@
 var black:FlxSprite; // It's not completely black, but weell... -EstoyAburridow
+var blackVignette:FlxSprite;
 
 function create()
 {
@@ -8,9 +9,36 @@ function create()
         black.makeSolid(FlxG.width, FlxG.height, 0xFF000000);
         black.cameras = [camHUD];
         insert(0, black);
+
+        stage.stageSprites["BlackVignette"].visible = stage.stageSprites["BlackVignette"].active = true;
+        stage.stageSprites["BlackVignette"].alpha = 0;
+
+        stage.stageSprites["BlackVignette"].drawComplex(FlxG.camera);
+        stage.stageSprites["BlackVignette"].cameras = [camHUD];
     }
     else
         __script__.didLoad = __script__.active = false;
+}
+
+function tweenHUD(fadeIn:Bool, time:Float){ //this is so i dont have to write this every time and also because i cannot use camhud or the cinematic bars will fuck up
+    for(i=>strumNote in player.members){
+        FlxTween.tween(strumNote,{alpha: fadeIn ? 1 : 0},time);
+    }
+    for(i=>strumNote in cpu.members){
+        FlxTween.tween(strumNote,{alpha: fadeIn ? 1 : 0},time);
+    }
+
+    FlxTween.tween(gorefieldhealthBarBG,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(gorefieldhealthBar,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(gorefieldiconP1,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(gorefieldiconP2,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(scoreTxt,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(missesTxt,{alpha: fadeIn ? 1 : 0},time);
+    FlxTween.tween(accuracyTxt,{alpha: fadeIn ? 1 : 0},time);
+}
+var dadXOffset:Float;
+function postCreate(){
+    dadXOffset = dad.cameraOffset.x;
 }
 
 function stepHit(step:Int) 
@@ -19,8 +47,21 @@ function stepHit(step:Int)
     {
         case 140:
             black.active = black.visible = false;
-            __script__.didLoad = __script__.active = false;
 
 			maxCamZoom = 0;
+        case 656:
+            tweenHUD(false,1);
+            lerpCam = false;
+            FlxTween.tween(FlxG.camera, {zoom: FlxG.camera.zoom + 0.4},1.7,{ease: FlxEase.backInOut, onComplete: (tmr:FlxTween) -> {
+                FlxTween.tween(FlxG.camera, {zoom: FlxG.camera.zoom + 0.7},13.5);
+            }});
+            dad.cameraOffset.x = dadXOffset - 260;
+            FlxTween.tween(stage.stageSprites["BlackVignette"], {alpha: 1},16);
+        case 780:
+            tweenHUD(true,0.3);
+            FlxTween.tween(stage.stageSprites["BlackVignette"], {alpha: 0},1);
+        case 783:
+            dad.cameraOffset.x = dadXOffset;
+            lerpCam = true;
     }
 }
