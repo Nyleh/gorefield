e8e163495eba38fda84a820febd4ada44b101fc1
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-24 15:58:05 -0700

    looping clouds in hi jon

--- data/stages/hijon.hx
+++ data/stages/hijon.hx
@@ -1,5 +1,20 @@
+//
+import flixel.addons.display.FlxBackdrop;
+
+var clouds:FlxBackdrop;
+var oldClouds:FlxSprite;
+
 function create() {
     comboGroup.x += 300;
     comboGroup.y += 300;
-    __script__.didLoad = __script__.active = false;
+
+    oldClouds = stage.stageSprites["cloudsFinal"];
+    oldClouds.alpha = 0;
+
+    clouds = new FlxBackdrop(Paths.image("stages/stage/clouds"), 0x01, 0, 0);
+    clouds.velocity.set(-150);
+
+    insert(members.indexOf(oldClouds), clouds);
 }
+
+function update(elapsed:Float) clouds.visible = oldClouds.visible;
