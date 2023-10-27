551ae1c45bbe76f07caa41becd5e9573d31cc31a
Lean <84594436+LeanDapper@users.noreply.github.com>
2023-10-26 19:22:57 -0700

    buh

--- songs/mondaylovania/scripts/song_events.hx
+++ songs/mondaylovania/scripts/song_events.hx
@@ -6,10 +6,15 @@ function create() {
 
 function stepHit(step:Int) {
     switch (step){
+        case 328: FlxTween.tween(camHUD, {alpha: 0}, (Conductor.stepCrochet / 1000) * 4);
+        case 332: FlxTween.tween(camHUD, {alpha: 1}, (Conductor.stepCrochet / 1000) * 3);
+        case 912: FlxTween.tween(camHUD, {alpha: 0}, (Conductor.stepCrochet / 1000) * 24);
+        case 972: FlxTween.tween(camHUD, {alpha: 1}, (Conductor.stepCrochet / 1000) * 4);
         case 1360:
         {
             gf.visible = gf.active = true;
-            FlxTween.tween(camHUD,{alpha: 0},2);
+            tweenHUD(0, (Conductor.stepCrochet / 1000) * 14);
         }
+        case 1488: camGame.alpha = camHUD.alpha = 0;
     }
 }
