bf63d29bbc0954f7cd49334b62c120df6b993013
Lean <84594436+LeanDapper@users.noreply.github.com>
2023-10-29 15:57:51 -0700

    cryfield icons and fixes

--- data/stages/cryfield.hx
+++ data/stages/cryfield.hx
@@ -1,8 +1,19 @@
 function create() {
     comboGroup.x += 300;
     comboGroup.y += 300;
-    gf.visible = false;
 
     gameOverSong = "gameOvers/cryfield/Gorefield_Gameover_Cryfield";
 	retrySFX = "gameOvers/cryfield/Continue";
+}
+
+function postCreate(){
+    if(PlayState.instance.SONG.meta.name == 'Nocturnal Meow'){
+        dad.setPosition(-400,-100);
+        boyfriend.setPosition(1900,700);
+        defaultCamZoom = 0.3;
+        dad.cameraOffset.x = 400;
+        strumLineDadZoom = 0.3;
+        strumLineBfZoom = 0.6;
+        snapCam();
+    }
 }
