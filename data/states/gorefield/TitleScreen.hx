20498ff82bbbca353c983f4fe40502927f847064
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-10-29 01:30:05 -0700

    clean up syntax for tittle

--- data/states/gorefield/TitleScreen.hx
+++ data/states/gorefield/TitleScreen.hx
@@ -1,6 +1,9 @@
 import flixel.text.FlxTextBorderStyle;
 import flixel.util.FlxAxes;
 
+static var skippedIntro:Bool = false;
+var transitioning:Bool = false;
+
 var house:FlxSprite;
 var logoBl:FlxSprite;
 var textGroup:FlxGroup;
@@ -70,152 +73,104 @@ function create() {
 	Conductor.changeBPM(96);
 }
 
-public function getIntroTextShit():Array<Array<String>>
-	{
-		var fullText:String = Assets.getText(Paths.txt('titlescreen/introText'));
+function getIntroTextShit():Array<Array<String>> {
+	var fullText:String = Assets.getText(Paths.txt('titlescreen/introText'));
 
-		var firstArray:Array<String> = fullText.split('\n');
-		var swagGoodArray:Array<Array<String>> = [];
+	var firstArray:Array<String> = fullText.split('\n');
+	var swagGoodArray:Array<Array<String>> = [];
 
-		for (i in firstArray)
-		{
-			swagGoodArray.push(i.split('--'));
-		}
+	for (i in firstArray)
+		swagGoodArray.push(i.split('--'));
+	return swagGoodArray;
+}
 
-		return swagGoodArray;
+function createCoolText(textArray:Array<String>) {
+	for (i => text in textArray) {
+		if (text == "" || text == null) continue;
+		var textShit = new FlxText(0, (i * 90) + 200, FlxG.width, text, 19, true);
+		textShit.setFormat("fonts/pixelart.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		textShit.scrollFactor.set();
+		textShit.screenCenter(FlxAxes.X);
+		textGroup.add(textShit);
 	}
+}
 
-public function createCoolText(textArray:Array<String>)
-	{
-		for (i=>text in textArray)
-		{
-			if (text == "" || text == null) continue;
-			var textShit = new FlxText(0, (i * 90) + 200, FlxG.width, text, 19, true);
-			textShit.setFormat("fonts/pixelart.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-			textShit.scrollFactor.set();
-			textShit.screenCenter(FlxAxes.X);
-			textGroup.add(textShit);
-		}
+function addMoreText(text:String) {
+	var moretextShit = new FlxText(0, (textGroup.length * 90) + 200, FlxG.width, text, 19, true);
+	moretextShit.setFormat("fonts/pixelart.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+	moretextShit.scrollFactor.set();
+	moretextShit.screenCenter(FlxAxes.X);
+	textGroup.add(moretextShit);
+}
+
+function deleteCoolText() {
+	while (textGroup.members.length > 0) {
+		textGroup.members[0].destroy();
+		textGroup.remove(textGroup.members[0], true);
 	}
+}
 
-public function addMoreText(text:String)
+function beatHit(curBeat:Int) {
+	logoBl.animation.play('bump',true);
+
+	FlxTween.tween(FlxG.camera, {zoom: 1.02}, 0.3, {ease: FlxEase.quadOut, type: FlxTween.BACKWARD});
+
+	if(skippedIntro) return;
+	switch (curBeat)
 	{
-		var moretextShit = new FlxText(0, (textGroup.length * 90) + 200, FlxG.width, text, 19, true);
-		moretextShit.setFormat("fonts/pixelart.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		moretextShit.scrollFactor.set();
-		moretextShit.screenCenter(FlxAxes.X);
-		textGroup.add(moretextShit);
+		case 2: createCoolText(["FNF' Gorefield Team"]);
+		case 4: addMoreText('Present');
+		case 5: deleteCoolText();
+		case 6: createCoolText(['In not association', 'with'], -40);
+		case 8: addMoreText('newgrounds', -40); ngSpr.visible = true;
+		case 9: deleteCoolText(); ngSpr.visible = false;
+		case 10: createCoolText(["Dathree estuvo aqui"]);
+		case 12: addMoreText("'Dathree'");
+		case 13: deleteCoolText();
+		case 14: createCoolText(["I don't speak spanish"]);
+		case 15: addMoreText("'Lean'");
+		case 16: deleteCoolText();
+		case 17: createCoolText(["Gorefield para ti BB"]);
+		case 18: addMoreText("'Nexus'");
+		case 19: deleteCoolText();
+		case 20: createCoolText(["Jon requiero enchiladas"]);
+		case 21: addMoreText("'Bitfox'");
+		case 22: deleteCoolText();
+		case 23: createCoolText(["Fifa 24"]);
+		case 24: addMoreText("'Jloor'");
+		case 25: deleteCoolText();
+		case 26: createCoolText(["JLoorcito fiu fiu"]);
+		case 27: addMoreText("'Zero'");
+		case 28: deleteCoolText();
+		case 29: createCoolText(["el peor mod de fnf"]);
+		case 30: addMoreText("'Keneth'");
+		case 31: deleteCoolText();
+		case 32: addMoreText("FNF'");
+		case 33: addMoreText('Vs Gorefield');
+		case 34: addMoreText('Part II');
+		case 35:
+			if(!FlxG.random.bool(10)) return;
+			deleteCoolText();
+			wiggleGorefield.visible = true;
+		case 36: skipIntro();
 	}
+}
 
-public function deleteCoolText()
-	{
-		while (textGroup.members.length > 0) {
-			textGroup.members[0].destroy();
-			textGroup.remove(textGroup.members[0], true);
-		}
+function skipIntro() {
+	if (!skippedIntro) {
+		remove(ngSpr);
+		remove(textGroup);
+		remove(blackScreen);
+		FlxG.camera.flash(FlxColor.WHITE, 2);
+		wiggleGorefield.visible = false;
+		skippedIntro = true;
+		FlxG.sound.playMusic(Paths.music('gorefield-menuLOOP'));
+		remove(vigentte); add(vigentte);
 	}
+}
 
-function beatHit(curBeat:Int)
-	{
-		logoBl.animation.play('bump',true);
 
-		FlxTween.tween(FlxG.camera, {zoom: 1.02}, 0.3, {ease: FlxEase.quadOut, type: FlxTween.BACKWARD});
-
-		if(skippedIntro) return;
-		switch (curBeat)
-		{
-			case 2:
-				createCoolText(["FNF' Gorefield Team"]);
-			case 4:
-				addMoreText('Present');
-			case 5:
-				deleteCoolText();
-			case 6:
-				createCoolText(['In not association', 'with'], -40);
-			case 8:
-				addMoreText('newgrounds', -40);
-				ngSpr.visible = true;
-			case 9:
-				deleteCoolText();
-				ngSpr.visible = false;
-			case 10:
-				createCoolText(["Dathree estuvo aqui"]);
-			case 12:
-				addMoreText("'Dathree'");
-			case 13:
-				deleteCoolText();
-			case 14:
-				createCoolText(["I don't speak spanish"]);
-			case 15:
-				addMoreText("'Lean'");
-			case 16:
-				deleteCoolText();
-			case 17:
-				createCoolText(["Gorefield para ti BB"]);
-			case 18:
-				addMoreText("'Nexus'");
-			case 19:
-				deleteCoolText();
-			case 20:
-				createCoolText(["Jon requiero enchiladas"]);
-			case 21:
-				addMoreText("'Bitfox'");
-			case 22:
-				deleteCoolText();
-			case 23:
-				createCoolText(["Fifa 24"]);
-			case 24:
-				addMoreText("'Jloor'");
-			case 25:
-				deleteCoolText();
-			case 26:
-				createCoolText(["JLoorcito fiu fiu"]);
-			case 27:
-				addMoreText("'Zero'");
-			case 28:
-				deleteCoolText();
-			case 29:
-				createCoolText(["el peor mod de fnf"]);
-			case 30:
-				addMoreText("'Keneth'");
-			case 31:
-				deleteCoolText();
-			case 32:
-				addMoreText("FNF'");
-			case 33:
-				addMoreText('Vs Gorefield');
-			case 34:
-				addMoreText('Part II');
-			case 35:
-				if(!FlxG.random.bool(10)) return;
-				deleteCoolText();
-				wiggleGorefield.visible = true;
-			case 36:
-				skipIntro();
-		}
-	}
-
-static var skippedIntro:Bool = false;
-
-function skipIntro():Void
-	{
-		if (!skippedIntro)
-		{
-			remove(ngSpr);
-			remove(textGroup);
-			remove(blackScreen);
-			FlxG.camera.flash(FlxColor.WHITE, 2);
-			wiggleGorefield.visible = false;
-			skippedIntro = true;
-			FlxG.sound.playMusic(Paths.music('gorefield-menuLOOP'));
-			remove(vigentte); add(vigentte);
-		}
-	}
-
-var transitioning:Bool = false;
-
-function update(elapsed:Float) 
-{
+function update(elapsed:Float) {
 	var pressedEnter:Bool = FlxG.keys.justPressed.ENTER;
 
 	if (FlxG.sound.music != null)
@@ -224,8 +179,7 @@ function update(elapsed:Float)
 	if (FlxG.keys.justPressed.SHIFT)
 		FlxG.switchState(new ModState("gorefield/easteregg/ArlenesCage"));
 
-	if (pressedEnter)
-	{
+	if (pressedEnter) {
 		if (!skippedIntro)
 			skipIntro();
 		else if (!transitioning)
