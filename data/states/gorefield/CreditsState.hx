ca8880913a5cbf13737ffc4d57ccaec943ac44f9
Lean <84594436+LeanDapper@users.noreply.github.com>
2023-10-29 14:50:36 -0700

    Update CreditsState.hx

--- data/states/gorefield/CreditsState.hx
+++ data/states/gorefield/CreditsState.hx
@@ -2,11 +2,14 @@ import flixel.FlxObject;
 import flixel.text.FlxTextBorderStyle;
 import flixel.text.FlxTextFormat;
 import flixel.text.FlxTextFormatMarkerPair;
+import funkin.backend.system.framerate.Framerate;
 
 var curSelected:Int = 0;
 var iconGroup:FlxTypedGroup<FlxSprite>;
 
 var descText:FlxText;
+var descText2:FlxText;
+var descTextName:FlxText;
 
 var credits:Array<Array<String>> = [
 	// Name's - Role - Random Description
@@ -17,7 +20,7 @@ var credits:Array<Array<String>> = [
 	["JoaDash", "Pixel Artist", "Matate oe, no trabaja"],
 	["Ani Manny", "Artist", "El de los chistes bien chistosos"],
 	["Lunar Cleint", "Coder", "El Mejor Coder de Todos los Tiempos (Messi)"],
-	["Lean", "Coder", "Lean..."],
+	["Lean", "Coder", "ummmm yeah i coded a lot of the mod"],
 	["EstoyAburridoW", "Coder", "Viciado en Celeste y viva Peru de nuevo"],
 	["Ne_Eo", "Coder", "Si"],
 	["Nexus Moon", "Musician", "El Undertale"],
@@ -26,17 +29,20 @@ var credits:Array<Array<String>> = [
 	["Deadshot", "Charter", "Otra vez, viva Peru!"],
 	["Tok", "Charter", "Se murio, no charteo"],
 	["KingFox", "Voice Actor", "El lobo de animal crossing!!!"],
-	["Lumpy Touch", "Gorefield Animation Creator", "El lobo de animal crossing!!!"],
-	["Jars Drawings", "Ultra Gorefield Design", "El lobo de animal crossing!!!"],
-	["Omega Black Art", "Ultra Gorefield Design", "El lobo de animal crossing!!!"],
-	["Aytanner", "Original Mondaylovania Musician", "El lobo de animal crossing!!!"]
+	["Lumpy Touch", "Gorefield Animation Creator", "The Lumpiest of Touches"],
+	["Jars Drawings", "Ultra Gorefield Design", "Sheesh"],
+	["Omega Black Art", "Ultra Gorefield Design", "Omega Art!!!!"],
+	["Aytanner", "Original Mondaylovania Musician", "El Undertale 2"]
 ];
 
 var camFollow:FlxObject;
 var camFollowPos:FlxObject;
 
+var iconYArray:Array<Float> = []; //intro stuff
+
 function create() 
 {
+	Framerate.instance.visible = false;
 	var background:FlxSprite = new FlxSprite();
 	background.loadGraphic(Paths.image("menus/credits/BG_0"));
 	background.setGraphicSize(FlxG.width, FlxG.height);
@@ -56,7 +62,7 @@ function create()
 	["Lumpy Touch", "Jars Drawings", "Omega Black Art", "Aytanner"]; //need to size these portraits to match the others
 	for (i in 0...credits.length)
 	{
-		var icon:FlxSprite = new FlxSprite(i % 3 * 370 + 80, Std.int(i / 3) * 420);
+		var icon:FlxSprite = new FlxSprite(i % 3 * 370 + 100, Std.int(i / 3) * 420);
 		icon.loadGraphic(Paths.image('menus/credits/' + credits[i][0]));
 		icon.setGraphicSize(332);
 		icon.ID = i;
@@ -69,6 +75,9 @@ function create()
 		}
 		icon.updateHitbox();
 		iconGroup.add(icon);
+		iconYArray.push(icon.y);
+
+		icon.y += 300;
 	}
 
 	FlxG.camera.follow(camFollowPos, null, 1);
@@ -79,14 +88,30 @@ function create()
 	descText.scrollFactor.set();
 	add(descText);
 
+	descTextName = new FlxText(32, 620, FlxG.width, "", 19, true);
+	descTextName.setFormat("fonts/pixelart.ttf", 50, FlxColor.WHITE, "center");
+	descTextName.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 5, 50);
+	descTextName.scrollFactor.set();
+	add(descTextName);
+
+	descText2 = new FlxText(32, descTextName.y + 50, FlxG.width, "", 19, true);
+	descText2.scrollFactor.set();
+	add(descText2);
+
 	var vigentte:FlxSprite = new FlxSprite().loadGraphic(Paths.image("menus/black_vignette"));
 	vigentte.alpha = 0.4; vigentte.scrollFactor.set(0,0);
 	add(vigentte);
 
+	for (i in 0...iconGroup.length){
+		FlxTween.tween(iconGroup.members[i], {y: iconYArray[i]}, 0.7,{ease: FlxEase.cubeOut, startDelay: 0.04 * i, onComplete: (tmr:FlxTween) -> {
+			intro = false;
+		}});
+	}
 	changeSelection(0);
 }
 
 var quitting:Bool = false;
+var intro:Bool = true;
 
 function update(elapsed:Float)
 {
@@ -120,11 +145,35 @@ function changeSelection(change:Int)
 
 	curSelected = FlxMath.wrap(curSelected + change, 0, credits.length-1);
 
-	var previousY = iconGroup.members[curSelected].y;
 	iconGroup.members[curSelected].alpha = 1;
-	camFollow.setPosition(640, Std.int(curSelected / 3) * 420 + 270);
-	iconGroup.members[curSelected].y = previousY + 20;
-	FlxTween.tween(iconGroup.members[curSelected],{y: previousY},0.3, {ease: FlxEase.backOut});
+	camFollow.setPosition(640, Std.int(curSelected / 3) * 420 + 230);
+	if(!intro)
+	{
+		var previousY = iconGroup.members[curSelected].y;
+		iconGroup.members[curSelected].y = previousY + 20;
+		FlxTween.tween(iconGroup.members[curSelected],{y: previousY},0.3, {ease: FlxEase.backOut});
+	}
 
-	descText.text = credits[curSelected][1];
+	var creditName:String = credits[curSelected][0];
+	var creditRole:String = credits[curSelected][1];
+	var creditDescription:String = credits[curSelected][2];
+
+	descText.text = creditRole;
+	descText2.text = creditDescription;
+	if(descText2.text.length > 55){
+		descText2.setFormat("fonts/pixelart.ttf", 20, 0xF2C0AC, "center");
+		descText2.y = descTextName.y + 55;
+	}
+	else{
+		descText2.setFormat("fonts/pixelart.ttf", 30, 0xF2C0AC, "center");
+		descText2.y = descTextName.y + 50;
+	}
+	descText2.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 3.2, 50);
+	descTextName.text = creditName;
+
+	descText.screenCenter(FlxAxes.X);
+	descText2.screenCenter(FlxAxes.X);
+	descTextName.screenCenter(FlxAxes.X);
 }
+
+function onDestroy() Framerate.instance.visible = true;
