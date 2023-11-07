c4a71c87c28a8f724d1eb00165e8b249931d2da1
EstoyAburridowGH <81833766+EstoyAburridowGH@users.noreply.github.com>
2023-11-06 22:00:03 -0500

    Cryfield stage
    I just try think creatively

--- data/stages/cryfield.hx
+++ data/stages/cryfield.hx
@@ -1,13 +1,52 @@
-function create() {
+var bloom:CustomShader;
+
+function create() 
+{
     comboGroup.x += 300;
     comboGroup.y += 300;
 
     gameOverSong = "gameOvers/cryfield/Gorefield_Gameover_Cryfield";
 	retrySFX = "gameOvers/cryfield/Continue";
+
+    if (PlayState.instance.SONG.meta.name == 'Nocturnal Meow')
+    {
+        stage.stageSprites["BG_C"].color = 0xFF7C7C7C;
+
+        for (char in [boyfriend, dad]) 
+        {
+            var newShader = new CustomShader("wrath");
+            newShader.uDirection = 45;
+            newShader.uOverlayOpacity = 0.5;
+            newShader.uDistance = 30.;
+            newShader.uChoke = 20.;
+            newShader.uPower = char == dad ? .2 : .7;
+        
+            newShader.uShadeColor = [219 / 255, 106 / 255, 104 / 255];
+            newShader.uOverlayColor = [41 / 255, 8 / 255, 33 / 255];
+        
+            var uv = char.frame.uv;
+            newShader.applyRect = [uv.x, uv.y, uv.width, uv.height];
+
+            //char.danceOnBeat = !(char.forceIsOnScreen = true);
+            if (FlxG.save.data.wrath) char.shader = newShader;
+        }
+    }
 }
 
-function postCreate(){
-    if(PlayState.instance.SONG.meta.name == 'Nocturnal Meow'){
+function draw(e) {
+	for (char in [boyfriend, dad]) 
+    {
+        if (char.shader == null) continue;
+
+        var uv = char.frame.uv;
+        char.shader.applyRect = [uv.x, uv.y, uv.width, uv.height];
+    }
+}
+
+function postCreate()
+{
+    if (PlayState.instance.SONG.meta.name == 'Nocturnal Meow')
+    {
         dad.setPosition(-400,-100);
         boyfriend.setPosition(1900,700);
         defaultCamZoom = 0.3;
