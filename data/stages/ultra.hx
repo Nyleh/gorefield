0a2f65e7855a5ffef931bab694ca28cdf3d1e19d
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2023-11-10 11:43:35 -0700

    i love skew

--- data/stages/ultra.hx
+++ data/stages/ultra.hx
@@ -1,9 +1,12 @@
 public var controlHealthAlpha:Bool = true;
 public var curHealthAlpha:Float = 1;
 
+var bg:FlxSprite;
 function create() {
     controlHealthAlpha = boyfriend.forceIsOnScreen = true; boyfriend.zoomFactor = 0.8;
     FlxG.camera.bgColor = 0xFFADABAB;
+
+    bg = stage.stageSprites["BG1"];
 }
 
 function onMeasureHit() {
@@ -11,6 +14,8 @@ function onMeasureHit() {
 }
 
 function update(elapsed:Float) {
+    bg.skew.y = lerp(bg.skew.y, curCameraTarget == 1 ? -8 : 0, 1/20);
+
     if (controlHealthAlpha) {
         curHealthAlpha = lerp(curHealthAlpha, curCameraTarget == 1 ? 0.25 : 1, 1/20);
         for (spr in [gorefieldhealthBarBG, gorefieldhealthBar, gorefieldiconP1, gorefieldiconP2, scoreTxt, missesTxt, accuracyTxt])
