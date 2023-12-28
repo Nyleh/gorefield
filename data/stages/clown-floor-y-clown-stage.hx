a07eef7314fdb4d090fa55386552cdadaf0fa2f4
Jloor <gamerjloor@gmail.com>
2023-12-28 00:15:07 -0500

    Add Song GameOver Clown Binky

--- data/stages/clown-floor-y-clown-stage.hx
+++ data/stages/clown-floor-y-clown-stage.hx
@@ -1,5 +1,15 @@
+
+
 function create()
 {
     var pixelScript = importScript("data/scripts/pixel-gorefield"); 
     pixelScript.set("noteType", "binky");
+
+    pixelScript.set("gameOverSong", "gameOvers/clown/CLOWN_GameOver_Loop");
+    pixelScript.set("retrySFX", "gameOvers/clown/CLOWN_GameOver_End");
+    pixelScript.set("lossSFX", "gameOverSFX");
+
+    gameOverSong = "gameOvers/clown/CLOWN_GameOver_Loop";
+    retrySFX = "gameOvers/clown/CLOWN_GameOver_End";
+    lossSFX = "gameOverSFX";
 }
