38cc79ebe128e68b786c44d297f8eaf42df0bddb
EstoyAburridowGH <81833766+EstoyAburridowGH@users.noreply.github.com>
2023-12-28 19:48:21 -0500

    Reset button fix | Pixel Lasagna stage countodwn | Ultra Gorefield animation fix

--- data/states/GameOverSubstate.hx
+++ data/states/GameOverSubstate.hx
@@ -3,55 +3,65 @@ import funkin.backend.utils.WindowUtils;
 
 var fakeCamFollow:FlxSprite;
 var replaceCamera:Bool = false;
-var characterDummy:Character;
+var daCharacter:Character;
 var fakeCamZoom:Float;
+var createNewCharacter:Bool = false;
 function create()
 {
     var position:Array<{x:Float,y:Float}> = [{x: 0, y: 0}];
 
+    createNewCharacter = PlayState.instance.boyfriend.gameOverCharacter != characterName;
+
+    daCharacter = new Character(PlayState.instance.boyfriend.x, PlayState.instance.boyfriend.y, PlayState.instance.boyfriend.gameOverCharacter, true);
+    if (createNewCharacter)
+    {
+        daCharacter.danceOnBeat = false;
+        daCharacter.playAnim('firstDeath');
+        add(daCharacter);
+    }
+
     fakeCamZoom = 1;
-    switch(characterName){
+    switch (daCharacter.curCharacter)
+    {
         case 'bf-apoc':
             fakeCamZoom = 0.6;
-            position[0].x = 160 + PlayState.instance.boyfriend.x;
-            position[0].y = 600 + PlayState.instance.boyfriend.y;  
+            position[0].x = 160 + daCharacter.x;
+            position[0].y = 600 + daCharacter.y;  
         case 'bf-ultra':
-            position[0].x = 200 + PlayState.instance.boyfriend.x;
-            position[0].y = 500 + PlayState.instance.boyfriend.y;  
+            position[0].x = 200 + daCharacter.x;
+            position[0].y = 500 + daCharacter.y;  
         case 'nermal-dead-cry':
             fakeCamZoom = 0.5;
-            position[0].x = 550 + PlayState.instance.boyfriend.x;
-            position[0].y = 240 + PlayState.instance.boyfriend.y;  
+            position[0].x = 550 + daCharacter.x;
+            position[0].y = 240 + daCharacter.y;  
         case 'bf-sky':
-            position[0].x = 70 + PlayState.instance.boyfriend.x;
-            position[0].y = 500 + PlayState.instance.boyfriend.y;  
+            position[0].x = 70 + daCharacter.x;
+            position[0].y = 500 + daCharacter.y;  
         case 'bf-black' | 'bf-black2':
-            var coolX = characterName == 'bf-black' ? 170 + PlayState.instance.boyfriend.x : 100 + PlayState.instance.boyfriend.x;
-            var coolY = characterName == 'bf-black' ? 500 + PlayState.instance.boyfriend.y : 400 + PlayState.instance.boyfriend.y;
+            var coolX = daCharacter.curCharacter == 'bf-black' ? 170 + daCharacter.x : 100 + daCharacter.x;
+            var coolY = daCharacter.curCharacter == 'bf-black' ? 500 + daCharacter.y : 400 + daCharacter.y;
             position[0].x = coolX;
             position[0].y = coolY; 
             fakeCamZoom = 0.7;
             snapCam();
         case 'bf-dead-bw':
-            position[0].x = 330 + PlayState.instance.boyfriend.x;
-            position[0].y = 600 + PlayState.instance.boyfriend.y;     
+            position[0].x = 330 + daCharacter.x;
+            position[0].y = 600 + daCharacter.y;     
         case 'jon-player':
-            position[0].x = 210 + PlayState.instance.boyfriend.x;
-            position[0].y = 550 + PlayState.instance.boyfriend.y;
+            position[0].x = 210 + daCharacter.x;
+            position[0].y = 550 + daCharacter.y;
         case 'bf-pixel-dead-lasagna':
             fakeCamZoom = 0.85;
-            position[0].x = 820 + PlayState.instance.boyfriend.x;
-            position[0].y = 425 + PlayState.instance.boyfriend.y;
+            position[0].x = 820 + daCharacter.x;
+            position[0].y = 425 + daCharacter.y;
             FlxG.camera.bgColor = 0xFF527f3a;
         case 'binky_game_over':
-            position[0].x = 1030 + PlayState.instance.boyfriend.x;
-            position[0].y = 1070 + PlayState.instance.boyfriend.y;
+            position[0].x = 1030 + daCharacter.x;
+            position[0].y = 1070 + daCharacter.y;
         default:
-            if (characterDummy == null)
-                characterDummy = new Character(0,0, characterName, true);
-            var camPos = characterDummy.getCameraPosition();
-            position[0].x = camPos.x + PlayState.instance.boyfriend.x;
-            position[0].y = camPos.y + PlayState.instance.boyfriend.y;
+            var camPos = daCharacter.getCameraPosition();
+            position[0].x = camPos.x + daCharacter.x;
+            position[0].y = camPos.y + daCharacter.y; 
     }
 
     fakeCamFollow = new FlxSprite(position[0].x, position[0].y).makeSolid(1, 1, 0xFFFFFFFF);
@@ -62,15 +72,51 @@ function create()
     WindowUtils.endfix = " - " + PlayState.instance.SONG.meta.name + " - GAME OVER";
 }
 
-function update(elapsed:Float){
+function postCreate()
+{
+    if (!createNewCharacter)
+        return;
+
+    for (member in members)
+        if (Std.isOfType(member, Character))
+        {
+            if (member == daCharacter)
+                continue;
+
+            remove(member);
+            break;
+        }
+}
+
+function beatHit(curBeat:Int)
+{
+    if (!createNewCharacter)
+        return;
+
+    if (FlxG.sound.music != null && FlxG.sound.music.playing)
+        daCharacter.playAnim("deathLoop", true);
+}
+
+function update(elapsed:Float)
+{
     FlxG.camera.zoom = lerp(FlxG.camera.zoom, fakeCamZoom, 0.05);
     if(replaceCamera){
         FlxG.camera.target = fakeCamFollow;
         replaceCamera = false;
     }
 
+    if (createNewCharacter)
+    {
+        if (controls.ACCEPT && !isEnding)
+            daCharacter.playAnim("deathConfirm", true);
+
+        if (!isEnding && ((!lossSFX.playing) || (daCharacter.getAnimName() == "firstDeath" && daCharacter.isAnimFinished())) && (FlxG.sound.music == null || !FlxG.sound.music.playing)) 
+        {
+            CoolUtil.playMusic(Paths.music(gameOverSong), false, 1, true, 100);
+            daCharacter.playAnim("deathLoop", true);
+        }
+    }
+
     if (controls.BACK)
-		{
-            isEnding = true; //fix that one music bug that codename somehow neglected
-		}
+        isEnding = true; //fix that one music bug that codename somehow neglected
 }
