0d1c75e2c296b4d3ce15f2baca1b56817042b55c
EstoyAburridowGH <81833766+EstoyAburridowGH@users.noreply.github.com>
2024-01-20 11:50:58 -0500

    Nocturnal Meow Intro

--- songs/nocturnal meow/scripts/song_events.hx
+++ songs/nocturnal meow/scripts/song_events.hx
@@ -1,8 +1,36 @@
 var scaryTime:Bool = false;
-function stepHit(step:Int) {
+
+var hudMembers:Array<FlxSprite>;
+
+function postCreate()
+{
+    hudMembers = [gorefieldhealthBarBG, gorefieldhealthBar, gorefieldiconP1, gorefieldiconP2, scoreTxt, missesTxt, accuracyTxt];
+    hudMembers = hudMembers.concat([for (note in cpuStrums.members) note]);
+    for (member in hudMembers)
+        member.visible = false;
+
+    boyfriend.cameraOffset.x += 20;
+    boyfriend.cameraOffset.y += 80;
+}
+
+function stepHit(step:Int) 
+{
     camGame.shake(scaryTime ? 0.006 : 0,0.1);
     camHUD.shake(scaryTime ? 0.005 : 0,0.1);
-    switch (step){
+    switch (step)
+    {
+        case 256:
+            strumLineBfZoom = 1.5;
+            boyfriend.cameraOffset.x -= 20;
+            boyfriend.cameraOffset.y -= 80;
+            showPopUp(true, PlayState.instance.misses);
+
+            for (member in hudMembers)
+            {
+                member.visible = true;
+                member.alpha = 0;
+                FlxTween.tween(member, {alpha: 1}, 0.4);
+            }
         case 1232:
             rain.visible = false;
             rain2.visible = scaryTime = true;
