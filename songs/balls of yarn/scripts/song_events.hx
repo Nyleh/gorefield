caa9077a1adeab9924525a48de779917f8e96cb5
Jloor <gamerjloor@gmail.com>
2024-01-31 17:13:24 -0500

    Update CC Stage and Add New Characters

--- songs/balls of yarn/scripts/song_events.hx
+++ songs/balls of yarn/scripts/song_events.hx
@@ -1,18 +1,19 @@
 function postCreate() 
 {
     camFollowChars = false; camFollow.setPosition(1500, 750);
+    snapCam();
 
     for (strum in strumLines) 
         for (char in strum.characters)
-            if (char.curCharacter == "pico") {
-                char.x += 300;
-                char.y -= 240;
+            if (char.curCharacter == "jon-cc") {
+                char.x += 450;
+                char.y -= 530;
             }
 
     for (strum in strumLines) 
         for (char in strum.characters)
-            if (char.curCharacter == "dad") {
-                char.x += 2800;
-                char.y += 600;
+            if (char.curCharacter == "cartoonfield") {
+                char.x += 1800;
+                char.y += 230;
             }
 }
