6315d695be77401d204433a92b273a741719539c
Lean <84594436+LeanDapper@users.noreply.github.com>
2024-02-05 21:07:59 -0800

    ok THERES THE THUNDER

--- songs/nocturnal meow/scripts/song_events.hx
+++ songs/nocturnal meow/scripts/song_events.hx
@@ -1,6 +1,14 @@
 var scaryTime:Bool = false;
 
 var hudMembers:Array<FlxSprite>;
+var bloom:CustomShader;
+
+function create(){
+    bloom = new CustomShader("glow");
+    bloom.size = 0.0;// trailBloom.quality = 8.0;
+    bloom.dim = 2;// trailBloom.directions = 16.0;
+    if (FlxG.save.data.bloom) {FlxG.camera.addShader(bloom); camHUD.addShader(bloom);}
+}
 
 function postCreate()
 {
@@ -15,8 +23,10 @@ function postCreate()
 
 function stepHit(step:Int) 
 {
-    camGame.shake(scaryTime ? 0.006 : 0,0.1);
-    camHUD.shake(scaryTime ? 0.005 : 0,0.1);
+    if(scaryTime){
+        camGame.shake(0.006,0.1);
+        camHUD.shake(0.005,0.1);
+    }
     switch (step)
     {
         case 256:
@@ -33,9 +43,23 @@ function stepHit(step:Int)
             }
         case 1232:
             rain.visible = false;
-            rain2.visible = scaryTime = true;
+            rain2.visible = true;
 
-            FlxG.camera.flash(0x808B3537, 1);
+            remove(stage.stageSprites["black"]);
+            insert(members.indexOf(stage.stageSprites["lightning_bolt"]) - 1, stage.stageSprites["black"]);
+
+            stage.stageSprites["black"].alpha = 0.8;
+            stage.stageSprites["black"].visible = stage.stageSprites["black"].active = true;
+            FlxTween.tween(stage.stageSprites["black"], {alpha: 0}, (Conductor.stepCrochet / 1000) * 6, {startDelay: (Conductor.stepCrochet / 1000) * 1});
+
+            camGame.shake(0.02,1);
+            camHUD.shake(0.02,1);
+
+            bloom.size = 8.0;
+            bloom.dim = 1.2;
+
+            FlxTween.num(8, 0, (Conductor.stepCrochet / 1000) * 16, {ease: FlxEase.quadInOut,startDelay: (Conductor.stepCrochet / 1000) * 1}, (val:Float) -> {bloom.size = val;});
+            FlxTween.num(1.2, 2, (Conductor.stepCrochet / 1000) * 16, {ease: FlxEase.quadInOut,startDelay: (Conductor.stepCrochet / 1000) * 1}, (val:Float) -> {bloom.dim = val;});
 
             var lightning_bolt:FlxSprite = stage.stageSprites["lightning_bolt"];
             lightning_bolt.visible = true;
@@ -45,6 +69,7 @@ function stepHit(step:Int)
                 {
                     lightning_bolt.animation.stop();
                     lightning_bolt.visible = lightning_bolt.active = false;
+                    scaryTime = true;
                 }
     }
 }
