7144fdf5457dcb22b8a86211af1eabb8f6ff775e
Jloor <gamerjloor@gmail.com>
2024-03-13 10:15:34 -0500

    Llevame contigo Jon

--- songs/captive/scripts/song_change.hx
+++ songs/captive/scripts/song_change.hx
@@ -7,18 +7,27 @@ function create()
     if (FlxG.save.data.warp) FlxG.game.addShader(distortionShader);
 }
 
+var boogie:Bool = false;
 function stepHit(step:Int) 
 {
     switch (step) {
         case 770:
             gf.visible = true;
             cpuStrums.visible = false;
+        case 1396:
+            boogie = false;
+        case 1412:
+            boogie = true;
         case 1280:
-            FlxTween.num(0, 1, (Conductor.stepCrochet / 1000) * 4, {}, (val:Float) -> {distortionShader.distortion = val;});
-        case 1410:
-            FlxTween.num(0, 2, (Conductor.stepCrochet / 1000) * 4, {}, (val:Float) -> {distortionShader.distortion = val;});
+            boogie = true;
         case 1542:
+            boogie = false;
             FlxTween.num(0, 0, (Conductor.stepCrochet / 1000) * 4, {}, (val:Float) -> {distortionShader.distortion = val;});
+    }
+
+    if (boogie && step % 4 == 0) 
+    {
+        FlxTween.num(0, 1, 0.1, {}, (val:Float) -> {distortionShader.distortion = val;}); if (curStep > 1410) FlxTween.num(0, 2, 0.1, {}, (val:Float) -> {distortionShader.distortion = val;});
     }
 }
 
