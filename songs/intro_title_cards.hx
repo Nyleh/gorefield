ea49271fd97c26c06152338fcf9e9c117777f61c
Lean <84594436+LeanDapper@users.noreply.github.com>
2024-03-12 17:10:51 -0700

    captive and balls of yarn intro card

--- songs/intro_title_cards.hx
+++ songs/intro_title_cards.hx
@@ -5,6 +5,8 @@ var data:T;
 
 var previousCardY:Float = 0;
 var slideUp:Bool = false;
+var slideDown:Bool = false;
+var changeAlpha:Bool = true;
 function postCreate() {
     var path:String = "assets/songs/" + SONG.meta.name.toLowerCase() + "/intro-title-card.json";
     if (!Assets.exists(path)) { disableScript(); return; }
@@ -24,19 +26,23 @@ function postCreate() {
         card.y += data.offset[1];
     }
     if (data.slideUp != null) slideUp = data.slideUp;
+    if (data.slideDown != null) slideDown = data.slideDown;
+    if (data.changeAlpha != null) changeAlpha = data.changeAlpha;
     previousCardY = card.y;
-    card.alpha = 0;
+    card.alpha = changeAlpha ? 0 : 1;
     add(card);
 
-    if(slideUp)
-        card.y += 350;
+    if(slideUp){
+        card.y += 350;}
+    else if(slideDown){
+        card.y -= 600;}
 }
 
 function onSongStart() {
-    FlxTween.tween(card, {alpha: 1, y: previousCardY}, slideUp ? 1 : 0.5, {startDelay: data.startDelay ?? 0, ease: FlxEase.cubeInOut});
+    FlxTween.tween(card, {alpha: 1, y: previousCardY}, slideUp || slideDown ? 1 : 0.5, {startDelay: data.startDelay ?? 0, ease: FlxEase.cubeInOut});
 
     var sixthOfDuration:Float = (data.duration ?? 2.5) / 5;
-    FlxTween.tween(card, {alpha: 0, y: previousCardY + (slideUp ? 400 : 0)}, sixthOfDuration * (slideUp ? 3 : 1),
+    FlxTween.tween(card, {alpha: changeAlpha ? 0 : 1, y: previousCardY + (slideUp ? 400 : slideDown ? -600 : 0)}, sixthOfDuration * (slideUp || slideDown ? 3 : 1),
     {
         startDelay: (data.startDelay ?? 0) + 0.5 + sixthOfDuration * 4,
         ease: FlxEase.cubeInOut,
