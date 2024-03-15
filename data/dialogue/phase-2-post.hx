3227947ba8d30846268ade6eac36eff58a700a56
EstoyAburridowGH <81833766+EstoyAburridowGH@users.noreply.github.com>
2024-03-14 19:27:10 -0500

    Arlene Dialogues translations phase 2 (3?)

--- data/dialogue/phase-2-post.hx
+++ data/dialogue/phase-2-post.hx
@@ -10,7 +10,7 @@ function create() {
     dialscript.dialogueList = [
         {
             message_en: "Back so soon??&& %I'm starting to think you just didn't even look...", 
-            message_es: "spanish text here", 
+            message_es: "Volviste tan pronto??&& %Estoy empezando a creer que ni siquiera lo buscaste...", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){
@@ -21,7 +21,7 @@ function create() {
         },
         {
             message_en: "%While you were gone,& I think I finally found out what the clown meant by highscore!", 
-            message_es: "spanish text here", 
+            message_es: "%Mientras no estabas,& creo que finalmente entendí lo que el payaso decia acerca de la mejor puntuación!", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){
@@ -32,7 +32,7 @@ function create() {
         },
         {
             message_en: "%Highscore is something inside video games...&&\nLike Garfield Kart!!!! &&(Yeah,& my boyfriend is famous)", 
-            message_es: "spanish text here", 
+            message_es: "%Las puntuaciones suelen ser algo de video juegos...&&\nComo Garfield Kart!!!! &&(Si,& mi novio es famoso)", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int) {
@@ -43,14 +43,14 @@ function create() {
         },
         {
             message_en: "Remember,& the second number of the code is the most repeated digit of your highscore...", 
-            message_es: "spanish text here", 
+            message_es: "Recuerda,& el segundo número del código es el digito más repetido de tu mejor puntuación...", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){}
         },
         {
             message_en: "I don't know where your gonna find a highscore,&&\n%Life isn't a game...", 
-            message_es: "spanish text here", 
+            message_es: "No sé donde vas a encontrar una mejor puntuación,&&\n%La vida no es un juego...", 
             typingSpeed: 0.05, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){
@@ -61,7 +61,7 @@ function create() {
         },
         {
             message_en: "%Also...&& You didn't even find the joke!!", 
-            message_es: "spanish text here", 
+            message_es: "%Además...&& Ni siquiera encontraste el chiste!!", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){
@@ -72,7 +72,7 @@ function create() {
         },
         {
             message_en: "%How am I meant to read you a joke if you didn't even find it?!", 
-            message_es: "spanish text here", 
+            message_es: "%Como se supone que voy a leerte un chiste si ni siquiera la has encontrado?!", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int) {
@@ -83,7 +83,7 @@ function create() {
         },
         {
             message_en: "%You know... &%Why am I even reading you these jokes in the first place??", 
-            message_es: "spanish text here", 
+            message_es: "%Sabes... &%Por qué si quiera estoy leyendote estos chistes en primer lugar??", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
                 event: function (count:Int) {
@@ -95,7 +95,7 @@ function create() {
         },
         {
             message_en: "Can't you read? &&%You were speaking fluent english just a moment ago...", 
-            message_es: "spanish text here", 
+            message_es: "No sabes leer? &&%Estabas hablando un inglés fluido hace un momento...", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int) {
@@ -106,7 +106,7 @@ function create() {
         },
         {
             message_en: "%Either way...&\nReminder,& the note is in a rain alleyway.&&\nThe clown left it for a crying cat.", 
-            message_es: "spanish text here", 
+            message_es: "%De cualquier manera...&\nRecuerda,& la nota está en un callejón lluvioso.&&\nEl payaso se lo dejó a un gato llorón.", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int) {
@@ -117,7 +117,7 @@ function create() {
         },
         {
             message_en: "Go find it so we can continue hunting for more jokes...", 
-            message_es: "spanish text here", 
+            message_es: "Ve y buscala entonces podremos seguir buscando por más chistes...", 
             typingSpeed: 0.055, startDelay: 0,
             onEnd: function () {},
             event: function (count:Int){}
